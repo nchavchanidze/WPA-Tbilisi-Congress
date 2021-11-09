363733b8fc1c56d69cae004ad7c8956f0efb70f5
Nikoloz Tchavtchanidze <nikolozchavchanidze@gmail.com>
2021-11-09 18:54:14 +0400

    Update FAQ

--- src/Components/FAQ.jsx
+++ src/Components/FAQ.jsx
@@ -4,7 +4,7 @@ import { Container, Dropdown } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
-import Background from "../Assets/Images/FAQ/bg.png";
+import Background from "../Assets/Images/Home/FAQ/bg.png";
 
 const FAQ = () => {
   return (
